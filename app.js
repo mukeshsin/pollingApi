cfca2823842ec934970f7b611d041dd516bc71b8
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-11 16:39:37 +0530

    allow access cors on origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json())
-app.use(cors({ origin: "http://localhost:8080" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 try {
   await db.authenticate();
   console.log("Connection has been established successfully.");
